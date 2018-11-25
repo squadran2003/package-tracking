13a1746bdfdc6f272b90acac487674a54f17b2ef
Andreas Cormack <cormackandy@hotmail.com>
2018-11-25 19:38:06 +0000

    added some templates and styling

--- package_tracking/package_tracking/urls.py
+++ package_tracking/package_tracking/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home')
 ]
+urlpatterns += staticfiles_urlpatterns()
