a27df856657a0c27149e5253eb93c9d612521889
Andreas Cormack <cormackandy@hotmail.com>
2018-12-09 20:26:26 +0000

    added a login route

--- package_tracking/package_tracking/urls.py
+++ package_tracking/package_tracking/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index, name='home')
 ]
 urlpatterns += staticfiles_urlpatterns()
